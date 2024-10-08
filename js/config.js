bba8a3d906ac631fb7b41e95353f312cf9475e9d
brian <103637120+bobdouceur2@users.noreply.github.com>
2024-10-08 17:26:19 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_Bq7IxWKCB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3odv4eg76f76olrs9bes9b22jp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
